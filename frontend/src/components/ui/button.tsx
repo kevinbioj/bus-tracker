08004330509ae9345e7cbe9e7074e08c34c2f2bf
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-01-22 21:20:59 +0100

    update dependencies

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -31,7 +31,6 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    // @ts-expect-error ...
     return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
   },
 );
