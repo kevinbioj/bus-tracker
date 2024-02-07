f3230feaf88969f28b58ae467c41f4a52fe60c94
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-02-07 21:21:59 +0100

    girouettes sngo complètes

--- frontend/src/data/sngo-routes.ts
+++ frontend/src/data/sngo-routes.ts
@@ -6,7 +6,44 @@ const sngoRoutes: Route[] = [
     mapOnly: true,
     name: "1",
     colors: { background: "#330613", text: "#FFFFFF" },
-    destinations: [],
+    destinations: [
+      {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#330613", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+      {
+        id: ["Sente du Bon Dieu"],
+        name: "Sente du Bon Dieu",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#330613", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          pages: [{ text: "VERNON" }, { font: "1508C2E1", text: "SENTE DU BON DIEU" }],
+        },
+      },
+      {
+        id: ["St Adjutor"],
+        name: "Saint-Adjutor",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#330613", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          pages: [{ text: "VERNON" }, { text: "SAINT-ADJUTOR" }],
+        },
+      },
+      {
+        id: ["VERNON Auges"],
+        name: "Auges",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#330613", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          pages: [{ text: "VERNON" }, { text: "AUGES" }],
+        },
+      },
+    ],
   },
   {
     id: "SNGO-22",
@@ -14,6 +51,33 @@ const sngoRoutes: Route[] = [
     name: "2",
     colors: { background: "#76B82A", text: "#FFFFFF" },
     destinations: [
+      {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#76B82A", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+      {
+        id: ["Lycée Dumézil"],
+        name: "Lycée Dumézil",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#76B82A", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
+          pages: [{ text: "VERNON" }, { text: "LYCEE DUMEZIL" }],
+        },
+      },
+      {
+        id: ["Blanchères"],
+        name: "Blanchères",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#76B82A", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
+          pages: [{ text: "VERNON" }, { text: "BLANCHERES" }],
+        },
+      },
       {
         id: ["VERNON Les Douers"],
         name: "Les Douers",
@@ -32,6 +96,24 @@ const sngoRoutes: Route[] = [
     colors: { background: "#FFDD00", text: "#000000" },
     destinations: [
       {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#FFDD00", textColor: "#000000", text: "3" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+      {
+        id: ["Clermont"],
+        name: "Clermont",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#FFDD00", textColor: "#000000", text: "3" },
+          pages: [{ text: "VERNON" }, { text: "CLERMONT" }],
+        },
+      },
+      {
         id: ["VERNON L. Damasse"],
         name: "Louis Damasse",
         city: "VERNON",
@@ -48,6 +130,15 @@ const sngoRoutes: Route[] = [
     name: "4",
     colors: { background: "#00B1EB", text: "#FFFFFF" },
     destinations: [
+      {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#00B1EB", textColor: "#FFFFFF", outlineColor: "#000000", text: "4" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
       {
         id: ["VERNON Corvette"],
         name: "Corvette",
@@ -66,6 +157,15 @@ const sngoRoutes: Route[] = [
     colors: { background: "#164194", text: "#FFFFFF" },
     destinations: [
       {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#164194", textColor: "#FFFFFF", outlineColor: "#000000", text: "5" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+      {
         id: ["SAINT MARCEL 11 Novembre"],
         name: "11 Novembre",
         city: "SAINT-MARCEL",
@@ -83,21 +183,21 @@ const sngoRoutes: Route[] = [
     colors: { background: "#EA619F", text: "#FFFFFF" },
     destinations: [
       {
-        id: ["SAINT-MARCEL Cornouillères"],
-        name: "Cornouillères",
-        city: "SAINT-MARCEL",
-        girouette: {
-          routeNumber: { backgroundColor: "#EA619F", textColor: "#FFFFFF", outlineColor: "#000000", text: "6" },
-          pages: [{ text: "SAINT-MARCEL" }, { text: "CORNOUILLERES" }],
-        },
-      },
-      {
         id: ["VERNON République"],
         name: "République",
         city: "VERNON",
         girouette: {
           routeNumber: { backgroundColor: "#EA619F", textColor: "#FFFFFF", outlineColor: "#000000", text: "6" },
           pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+      {
+        id: ["SAINT-MARCEL Cornouillères"],
+        name: "Cornouillères",
+        city: "SAINT-MARCEL",
+        girouette: {
+          routeNumber: { backgroundColor: "#EA619F", textColor: "#FFFFFF", outlineColor: "#000000", text: "6" },
+          pages: [{ text: "SAINT-MARCEL" }, { text: "CORNOUILLERES" }],
         },
       },
     ],
@@ -107,21 +207,122 @@ const sngoRoutes: Route[] = [
     mapOnly: true,
     name: "8A",
     colors: { background: "#8C663A", text: "#FFFFFF" },
-    destinations: [],
+    destinations: [
+      {
+        id: ["Lycée Jean Moulin"],
+        name: "Lycée Jean Moulin",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#8C663A", textColor: "#FFFFFF", outlineColor: "#000000", text: "8A" },
+          pages: [{ text: "LES ANDELYS" }, { font: "1508C2E1", text: "LYCEE JEAN MOULIN" }],
+        },
+      },
+      {
+        id: ["Radeval"],
+        name: "Radeval",
+        girouette: {
+          routeNumber: { backgroundColor: "#8C663A", textColor: "#FFFFFF", outlineColor: "#000000", text: "8A" },
+          pages: [{ text: "RADEVAL" }],
+        },
+      },
+      {
+        id: ["Gare Routière"],
+        name: "Gare Routière",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#8C663A", textColor: "#FFFFFF", outlineColor: "#000000", text: "8A" },
+          pages: [{ text: "LES ANDELYS" }, { text: "GARE ROUTIERE" }],
+        },
+      },
+      {
+        id: ["Saint Jacques"],
+        name: "Saint-Jacques",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#8C663A", textColor: "#FFFFFF", outlineColor: "#000000", text: "8A" },
+          pages: [{ text: "LES ANDELYS" }, { text: "SAINT-JACQUES" }],
+        },
+      },
+    ],
   },
   {
     id: "SNGO-48",
     mapOnly: true,
     name: "8B",
     colors: { background: "#CDCC0B", text: "#FFFFFF" },
-    destinations: [],
+    destinations: [
+      {
+        id: ["Feuquerolles"],
+        name: "Feuquerolles",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#CDCC0B", textColor: "#FFFFFF", outlineColor: "#000000", text: "8B" },
+          pages: [{ text: "LES ANDELYS" }, { text: "FEUQUEROLLES" }],
+        },
+      },
+      {
+        id: ["Gare Routière"],
+        name: "Gare Routière",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#CDCC0B", textColor: "#FFFFFF", outlineColor: "#000000", text: "8B" },
+          pages: [{ text: "LES ANDELYS" }, { text: "GARE ROUTIERE" }],
+        },
+      },
+      {
+        id: ["Longuemare"],
+        name: "Longuemare",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#CDCC0B", textColor: "#FFFFFF", outlineColor: "#000000", text: "8B" },
+          pages: [{ text: "LES ANDELYS" }, { text: "LONGUEMARE" }],
+        },
+      },
+      {
+        id: ["Lycée Jean Moulin"],
+        name: "Lycée Jean Moulin",
+        city: "LES ANDELYS",
+        girouette: {
+          routeNumber: { backgroundColor: "#CDCC0B", textColor: "#FFFFFF", outlineColor: "#000000", text: "8B" },
+          pages: [{ text: "LES ANDELYS" }, { font: "1508C2E1", text: "LYCEE JEAN MOULIN" }],
+        },
+      },
+      {
+        id: ["ST-JEAN DE FRENELLES Rond Point"],
+        name: "Rond Point",
+        city: "Saint-Jean de Frenelles",
+        girouette: {
+          routeNumber: { backgroundColor: "#CDCC0B", textColor: "#FFFFFF", outlineColor: "#000000", text: "8B" },
+          pages: [{ text: ["SAINT-JEAN", "DE FRENELLES"] }, { text: "ROND POINT" }],
+        },
+      },
+    ],
   },
   {
     id: "SNGO-29",
     mapOnly: true,
     name: "9",
     colors: { background: "#7F5DA4", text: "#FFFFFF" },
-    destinations: [],
+    destinations: [
+      {
+        id: ["MENILLES Haut Ménilles"],
+        name: "Haut Ménilles",
+        city: "MENILLES",
+        girouette: {
+          routeNumber: { backgroundColor: "#7F5DA4", textColor: "#FFFFFF", outlineColor: "#000000", text: "9" },
+          pages: [{ text: "MENILLES" }, { text: "HAUT MENILLES" }],
+        },
+      },
+      {
+        id: ["PACY Zone Artisanale"],
+        name: "Zone Artisanale",
+        city: "PACY-SUR-EURE",
+        girouette: {
+          routeNumber: { backgroundColor: "#7F5DA4", textColor: "#FFFFFF", outlineColor: "#000000", text: "9" },
+          pages: [{ text: "PACY-SUR-EURE" }, { font: "1510N2E1", text: "ZONE ARTISANALE" }],
+        },
+      },
+    ],
   },
   {
     id: "SNGO-10",
@@ -138,6 +339,15 @@ const sngoRoutes: Route[] = [
           pages: [{ text: "ECOS" }, { font: "1510N2E1", textSpacing: 1, text: "PLACE DE L'EGLISE" }],
         },
       },
+      {
+        id: ["ECOS Place de l'Eglise"],
+        name: "Place de l'Eglise",
+        city: "ECOS",
+        girouette: {
+          routeNumber: { backgroundColor: "#EF7D00", textColor: "#FFFFFF", outlineColor: "#000000", text: "10" },
+          pages: [{ text: "ECOS" }, { font: "1510N2E1", textSpacing: 1, text: "PLACE DE L'EGLISE" }],
+        },
+      },
     ],
   },
   {
@@ -145,7 +355,35 @@ const sngoRoutes: Route[] = [
     mapOnly: true,
     name: "11",
     colors: { background: "#252B5A", text: "#FFFFFF" },
-    destinations: [],
+    destinations: [
+      {
+        id: ["DOUAINS Normandie Parc Nord"],
+        name: "Normandie Parc Nord",
+        city: "DOUAINS",
+        girouette: {
+          routeNumber: { backgroundColor: "#EF7D00", textColor: "#FFFFFF", outlineColor: "#000000", text: "11" },
+          pages: [{ text: "DOUAINS" }, { font: "1508C2E1", text: "NORMANDIE PARC NORD" }],
+        },
+      },
+      {
+        id: ["PACY SUR EURE Gare Routière"],
+        name: "Gare Routière",
+        city: "PACY-SUR-EURE",
+        girouette: {
+          routeNumber: { backgroundColor: "#EF7D00", textColor: "#FFFFFF", outlineColor: "#000000", text: "11" },
+          pages: [{ text: "PACY-SUR-EURE" }, { text: "GARE ROUTIERE" }],
+        },
+      },
+      {
+        id: ["VERNON République"],
+        name: "République",
+        city: "VERNON",
+        girouette: {
+          routeNumber: { backgroundColor: "#EF7D00", textColor: "#FFFFFF", outlineColor: "#000000", text: "11" },
+          pages: [{ text: "VERNON" }, { text: "REPUBLIQUE" }],
+        },
+      },
+    ],
   },
 ];
 
