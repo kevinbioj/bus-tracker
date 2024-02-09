0a3ceae28774274ccd6b17335bf5fb3f85078c8b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-02-09 08:50:52 +0100

    save location for future visits

--- frontend/src/components/interactive-map/interactive-map.tsx
+++ frontend/src/components/interactive-map/interactive-map.tsx
@@ -2,8 +2,10 @@
 
 import { LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { useLocalStorage } from "usehooks-ts";
 
 import Geolocate from "~/components/interactive-map/geolocate";
+import LocationSaver from "~/components/interactive-map/location-saver";
 
 import VehicleHightlight from "./vehicle-highlight";
 import VehicleMarkers from "./vehicle-markers";
@@ -14,14 +16,21 @@ type InteractiveMapProps = {
 };
 
 export default function InteractiveMap({ center, className }: InteractiveMapProps) {
+  const [lastLocation] = useLocalStorage<[number, number, number] | null>("last-location", null);
   return (
     <section>
       <h2 className="font-medium sr-only text-2xl text-center">Carte interactive</h2>
-      <MapContainer center={center} className={className} id="interactive-map" zoom={12}>
+      <MapContainer
+        center={lastLocation ? [lastLocation[0], lastLocation[1]] : center}
+        className={className}
+        id="interactive-map"
+        zoom={lastLocation?.[2] ?? 12}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <LocationSaver />
         <VehicleMarkers />
         <VehicleHightlight />
         <Geolocate />
