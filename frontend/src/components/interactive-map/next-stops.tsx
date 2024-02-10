c6b05438d86fc557436584036068f658a7433a50
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-02-10 16:21:31 +0100

    fix next stops on Chromium browsers

--- frontend/src/components/interactive-map/next-stops.tsx
+++ frontend/src/components/interactive-map/next-stops.tsx
@@ -9,21 +9,16 @@ type NextStopsProps = { stopTimes: StopTime[]; width: number };
 export default function NextStops({ stopTimes, width }: NextStopsProps) {
   if (stopTimes.length === 0) return null;
   return (
-    <div className="mt-0.5 -mb-1.5 px-2">
-      <div className="font-[Rubik] max-h-24 min-w-44 mt-0.5 overflow-scroll w-full">
+    <div className="-mb-2 px-2">
+      <div className="font-[Rubik] max-h-24 min-w-44 mt-0.5 overflow-y-scroll w-full">
         {stopTimes.map((stopTime) => {
           return (
-            <div className="flex font-medium justify-between mt-0.5" key={stopTime.id}>
-              <div className="flex-1 flex justify-between">
-                <span
-                  className="mr-2 overflow-hidden text-ellipsis whitespace-nowrap"
-                  style={{ width: `${width - 16 - 38 - 8 - (stopTime.isRealtime ? 8 : 0)}px` }}
-                >
-                  {stopTime.name}
-                </span>
-                {stopTime.isRealtime && <Rss className="-rotate-90" color="green" size={8} />}
-              </div>
-              <div className="tabular-nums text-center w-[38px]">
+            <div className="grid grid-cols-[auto_1rem_2.35rem] font-medium mt-0.5" key={stopTime.id}>
+              <span className="overflow-hidden text-ellipsis whitespace-nowrap" title={stopTime.name}>
+                {stopTime.name}
+              </span>
+              {stopTime.isRealtime ? <Rss className="ml-auto -rotate-90" color="green" size={8} /> : <div></div>}
+              <div className="tabular-nums">
                 {match([stopTime.timestamp, stopTime.isRealtime])
                   .with([null, P.boolean], () => (
                     <X className="mx-auto -mt-0.5" color="red" size={18} strokeWidth={3} />
