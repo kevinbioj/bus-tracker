a887a841ef74d665944774f82d7db89a9df926c7
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-02-22 09:08:04 +0100

    fix: actually remove the right 530

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -90,7 +90,7 @@ const sources: Source[] = [
     gtfsProperties: {
       id: 'NOMAD',
       staticResourceHref: 'https://gtfs.kevinbioj.fr/nomad.zip',
-      filters: { scheduled: (trip) => trip.route !== '530' },
+      filters: { scheduled: (trip) => trip.route !== 'NOMAD-530' },
       getOperator: () => 'NOMAD',
     },
   },
