a80575f685a166571f93b37f093ebbfdab6f1f30
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-03-24 20:47:53 +0100

    neva: clean Neva routes

--- frontend/src/data/networks/neva.ts
+++ frontend/src/data/networks/neva.ts
@@ -4,88 +4,25 @@ const nevaRoutes: Route[] = [
   {
     id: "NEVA:zenbus:Line:350210009:LOC",
     name: "1",
-    mapOnly: true,
     noCartridge: true,
-    colors: { background: "#76FF03", text: "#FFFFFF" },
+    colors: { background: "#76FF03", text: "#000000" },
     destinations: [
-      {
-        id: ["ZA Prétôt (2)"],
-        name: "ZA Prétôt (2)",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "Z.A. PRETOT" }],
-        },
-      },
-      {
-        id: ["CÉRENCES - Square de la Gare"],
-        name: "CÉRENCES - Square de la Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "CÉRENCES" }, { font: "1508C2E1", textSpacing: 3, text: "SQUARE DE LA GARE" }],
-        },
-      },
-      {
-        id: ["Hauteville (2)"],
-        name: "Hauteville (2)",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "HAUTEVILLE" }],
-        },
-      },
-      {
-        id: ["Haute-Lande (1)"],
-        name: "Haute-Lande (1)",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "HAUTE-LANDE" }],
-        },
-      },
       {
         id: ["Les Cèdres"],
         name: "Les Cèdres",
+        city: "YQUELON",
         girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          routeNumber: { backgroundColor: "#76FF03", textColor: "#000000", outlineColor: "#FFFFFF", text: "1" },
           pages: [{ text: "LES CEDRES" }],
         },
       },
       {
         id: ["Cours Jonville"],
         name: "Cours Jonville",
+        city: "GRANVILLE",
         girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
+          routeNumber: { backgroundColor: "#76FF03", textColor: "#000000", outlineColor: "#FFFFFF", text: "1" },
           pages: [{ font: "1510N2E1", text: "COURS JONVILLE" }],
-        },
-      },
-      {
-        id: ["ZA Croissant"],
-        name: "ZA Croissant",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "Z.A. CROISSANT" }],
-        },
-      },
-      {
-        id: ["Granville - ZA TAILLAIS"],
-        name: "Granville - ZA TAILLAIS",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: "Z.A. TAILLAIS" }],
-        },
-      },
-      {
-        id: ["La Haye-Pesnel - COLLEGE"],
-        name: "La Haye-Pesnel - COLLEGE",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ text: ["LA HAYE-PESNEL", "COLLEGE"] }],
-        },
-      },
-      {
-        id: ["GRANVILLE - Gare"],
-        name: "GRANVILLE - Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#76FF03", textColor: "#FFFFFF", outlineColor: "#000000", text: "1" },
-          pages: [{ font: "1510N2E1", text: "GRANVILLE GARE" }],
         },
       },
     ],
@@ -93,88 +30,25 @@ const nevaRoutes: Route[] = [
   {
     id: "NEVA:zenbus:Line:348940035:LOC",
     name: "2",
-    mapOnly: true,
     noCartridge: true,
     colors: { background: "#E91E63", text: "#FFFFFF" },
     destinations: [
       {
-        id: ["ZA Prétôt (2)"],
-        name: "ZA Prétôt (2)",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: "Z.A. PRETOT" }],
-        },
-      },
-      {
-        id: ["CÉRENCES - Square de la Gare"],
-        name: "CÉRENCES - Square de la Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: "CÉRENCES" }, { font: "1508C2E1", textSpacing: 3, text: "SQUARE DE LA GARE" }],
-        },
-      },
-      {
-        id: ["Hauteville (2)"],
-        name: "Hauteville (2)",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: "HAUTEVILLE" }],
-        },
-      },
-      {
         id: ["Haute-Lande (1)"],
-        name: "Haute-Lande (1)",
+        name: "Haute-Lande",
+        city: "YQUELON",
         girouette: {
           routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
           pages: [{ text: "HAUTE-LANDE" }],
         },
       },
       {
-        id: ["Les Cèdres"],
-        name: "Les Cèdres",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: "LES CEDRES" }],
-        },
-      },
-      {
-        id: ["Cours Jonville"],
-        name: "Cours Jonville",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ font: "1510N2E1", text: "COURS JONVILLE" }],
-        },
-      },
-      {
         id: ["ZA Croissant"],
-        name: "ZA Croissant",
+        name: "Z.A. Croissant",
+        city: "SAINT-PAIR-SUR-MER",
         girouette: {
           routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
           pages: [{ text: "Z.A. CROISSANT" }],
-        },
-      },
-      {
-        id: ["Granville - ZA TAILLAIS"],
-        name: "Granville - ZA TAILLAIS",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: "Z.A. TAILLAIS" }],
-        },
-      },
-      {
-        id: ["La Haye-Pesnel - COLLEGE"],
-        name: "La Haye-Pesnel - COLLEGE",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ text: ["LA HAYE-PESNEL", "COLLEGE"] }],
-        },
-      },
-      {
-        id: ["GRANVILLE - Gare"],
-        name: "GRANVILLE - Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#E91E63", textColor: "#FFFFFF", outlineColor: "#000000", text: "2" },
-          pages: [{ font: "1510N2E1", text: "GRANVILLE GARE" }],
         },
       },
     ],
@@ -182,88 +56,25 @@ const nevaRoutes: Route[] = [
   {
     id: "NEVA:zenbus:Line:761900004:LOC",
     name: "3",
-    mapOnly: true,
     noCartridge: true,
     colors: { background: "#536DFE", text: "#FFFFFF" },
     destinations: [
       {
         id: ["ZA Prétôt (2)"],
-        name: "ZA Prétôt (2)",
+        name: "Z.A. Prétôt",
+        city: "GRANVILLE",
         girouette: {
           routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
           pages: [{ text: "Z.A. PRETOT" }],
         },
       },
       {
-        id: ["CÉRENCES - Square de la Gare"],
-        name: "CÉRENCES - Square de la Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: "CÉRENCES" }, { font: "1508C2E1", textSpacing: 3, text: "SQUARE DE LA GARE" }],
-        },
-      },
-      {
-        id: ["Hauteville (2)"],
-        name: "Hauteville (2)",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: "HAUTEVILLE" }],
-        },
-      },
-      {
         id: ["Haute-Lande (1)"],
-        name: "Haute-Lande (1)",
+        name: "Haute-Lande",
+        city: "GRANVILLE",
         girouette: {
           routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
           pages: [{ text: "HAUTE-LANDE" }],
-        },
-      },
-      {
-        id: ["Les Cèdres"],
-        name: "Les Cèdres",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: "LES CEDRES" }],
-        },
-      },
-      {
-        id: ["Cours Jonville"],
-        name: "Cours Jonville",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ font: "1510N2E1", text: "COURS JONVILLE" }],
-        },
-      },
-      {
-        id: ["ZA Croissant"],
-        name: "ZA Croissant",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: "Z.A. CROISSANT" }],
-        },
-      },
-      {
-        id: ["Granville - ZA TAILLAIS"],
-        name: "Granville - ZA TAILLAIS",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: "Z.A. TAILLAIS" }],
-        },
-      },
-      {
-        id: ["La Haye-Pesnel - COLLEGE"],
-        name: "La Haye-Pesnel - COLLEGE",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ text: ["LA HAYE-PESNEL", "COLLEGE"] }],
-        },
-      },
-      {
-        id: ["GRANVILLE - Gare"],
-        name: "GRANVILLE - Gare",
-        girouette: {
-          routeNumber: { backgroundColor: "#536DFE", textColor: "#FFFFFF", outlineColor: "#000000", text: "3" },
-          pages: [{ font: "1510N2E1", text: "GRANVILLE GARE" }],
         },
       },
     ],
