5af3cc68bab2af961235fc4c1ddc7be4d6c609dd
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-03-26 21:01:39 +0100

    feat: allow vehicles sorting by activity

--- frontend/src/app/vehicles/[operator]/VehicleList.tsx
+++ frontend/src/app/vehicles/[operator]/VehicleList.tsx
@@ -3,22 +3,37 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useMemo, useState } from "react";
-import { ArrowLeft, Filter } from "tabler-icons-react";
+import { ArrowBounce, ArrowLeft, ArrowLeftRight, ArrowsSort, Filter, Sort09 } from "tabler-icons-react";
 import { match } from "ts-pattern";
 
 import { LightVehicleDto, Operator } from "~/@types";
+import { Button } from "~/components/ui/button";
 import { VehicleCard } from "~/components/vehicle-history/VehicleCard";
 
 type VehicleList = { operator: Operator; vehicles: LightVehicleDto[] };
 
 export default function VehicleList({ operator, vehicles }: VehicleList) {
+  const [sort, setSort] = useState<"number" | "activity">("number");
   const [filter, setFilter] = useState("");
 
   const filtered = useMemo(() => {
-    if (filter === "") return vehicles;
     const pattern = new RegExp(filter.replaceAll("_", "\\d"));
-    return vehicles.filter((v) => pattern.test(v.number.toString()) || pattern.test(v.name ?? ""));
-  }, [filter, vehicles]);
+    return vehicles
+      .filter((v) => {
+        if (filter === "") return true;
+        return pattern.test(v.number.toString()) || pattern.test(v.name ?? "");
+      })
+      .sort((a, b) => {
+        if (sort === "number") {
+          return a.number - b.number;
+        } else if (sort === "activity") {
+          return (
+            (b.sinceTime ? new Date(b.sinceTime).getTime() : 0) - (a.sinceTime ? new Date(a.sinceTime).getTime() : 0)
+          );
+        }
+        return 0;
+      });
+  }, [filter, sort, vehicles]);
 
   const activeCount = useMemo(() => filtered.filter((v) => v.currentRouteId !== null).length, [filtered]);
 
@@ -32,7 +47,7 @@ export default function VehicleList({ operator, vehicles }: VehicleList) {
           width={operator.logo.size[0]}
           height={operator.logo.size[1]}
         />
-        <div className="flex flex-col sm:flex-row gap-1">
+        <div className="flex flex-col md:flex-row gap-1">
           <div className="flex flex-1 items-start gap-4">
             <Link
               className="bg-brand rounded-md text-white hover:brightness-90"
@@ -53,15 +68,23 @@ export default function VehicleList({ operator, vehicles }: VehicleList) {
               )}
             </div>
           </div>
-          <div className="flex items-start justify-end gap-2">
-            <Filter width={32} height={32} />
-            <input
-              className="bg-neutral-100 border-[1px] border-neutral-700 px-1 py-0.5 rounded-md w-full dark:bg-neutral-700 dark:border-neutral-300"
-              onChange={(e) => setFilter(e.target.value)}
-              placeholder="Filter les véhicules..."
-              type="text"
-              value={filter}
-            />
+          <div className="flex items-center justify-between gap-4">
+            <Button
+              className="flex gap-2 w-48"
+              onClick={() => setSort((s) => (s === "number" ? "activity" : "number"))}
+            >
+              <ArrowsSort /> Tri par {sort === "number" ? "n° de parc" : "activité"}
+            </Button>
+            <div className="flex items-start justify-end gap-2">
+              <Filter width={32} height={32} />
+              <input
+                className="bg-neutral-100 border-[1px] border-neutral-700 px-1 py-0.5 rounded-md w-full dark:bg-neutral-700 dark:border-neutral-300"
+                onChange={(e) => setFilter(e.target.value)}
+                placeholder="Filter les véhicules..."
+                type="text"
+                value={filter}
+              />
+            </div>
           </div>
         </div>
       </section>
