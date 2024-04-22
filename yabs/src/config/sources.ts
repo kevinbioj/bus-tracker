1065fa6ebef3a022809b2fd04a2e4eb812a7bcff
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-04-22 07:41:53 +0200

    deepmob: enable routes 500, 700 & 900

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -285,9 +285,6 @@ const sources: Source[] = [
       staticResourceHref: 'https://www.data.gouv.fr/fr/datasets/r/62248658-0eba-4f4e-b367-aaea635ecd38',
       tripUpdateHref: 'https://tud.geo3d.hanoverdisplays.com/api-1.0/gtfs-rt/trip-updates',
       vehiclePositionHref: 'https://tud.geo3d.hanoverdisplays.com/api-1.0/gtfs-rt/vehicle-positions',
-      filters: {
-        scheduled: (trip) => ['1', '2', '3', '97', '14'].includes(trip.route),
-      },
       getOperator: () => 'DEEPMOB',
     },
   },
