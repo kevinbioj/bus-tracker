44f40abf82de154eb912c0e273599cd952b48be3
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-04-29 20:41:26 +0200

    refactor: great optimization in terms of memory consumption

--- yabs/src/yabs/fetcher/gtfs/compute-entries.ts
+++ yabs/src/yabs/fetcher/gtfs/compute-entries.ts
@@ -5,9 +5,9 @@ import { YabsEntry } from '~/@types';
 import { checkCalendar } from '~/utils/check-calendar';
 import { checkTrip } from '~/utils/check-trip';
 import { parseTime } from '~/utils/parse-time';
+import { search } from '~/utils/search';
 import { GtfsProperties, GtfsResource } from '~/yabs/fetcher/gtfs/@types';
 import { decodeTripUpdate, decodeVehiclePosition } from '~/yabs/fetcher/gtfs/decode-gtfsrt';
-import { orm } from '~/yabs/vehicles/database';
 import { getVehicleLedColor } from '~/yabs/vehicles/get-vehicle-led-color';
 
 export async function computeGtfsEntries(resource: GtfsResource, properties: GtfsProperties): Promise<YabsEntry[]> {
@@ -40,13 +40,14 @@ export async function computeGtfsEntries(resource: GtfsResource, properties: Gtf
 export function computeScheduled(resource: GtfsResource, properties: GtfsProperties, processedTrips: string[] = []) {
   const filter = properties.filters?.scheduled;
 
-  const ongoingTrips = [...resource.trips.values()].filter((trip, index, array) => {
+  const ongoingTrips = resource.trips.filter((trip, index, array) => {
     // 1. We run through the custom filter first.
     if (typeof filter !== 'undefined' && !filter(trip, index, array, resource)) return false;
     // 2. If the trip was already seen in realtime data, we skip it.
     if (processedTrips.includes(trip.id)) return false;
     // 3. If a trip with the same block (if available) was processed, we skip it.
-    if (trip.block !== null && processedTrips.some((id) => resource.trips.get(id)!.block === trip.block)) return false;
+    if (trip.block !== null && processedTrips.some((id) => search(resource.trips, id)!.block === trip.block))
+      return false;
     // 4. We check for calendar and stop time.
     return checkCalendar(trip.calendar) && checkTrip(trip);
   });
@@ -62,9 +63,9 @@ export function computeScheduled(resource: GtfsResource, properties: GtfsPropert
       isRealtime: false,
     }));
 
-    const currentStopTime = [...stopTimes].reverse().find((stopTime) => Date.now() >= +stopTime.timestamp * 1000)!;
+    const currentStopTime = stopTimes.toReversed().find((stopTime) => Date.now() >= +stopTime.timestamp * 1000)!;
     const currentStopTimeIdx = stopTimes.indexOf(currentStopTime);
-    const currentStop = resource.stops.get(currentStopTime.id)!;
+    const currentStop = search(resource.stops, currentStopTime.id)!;
 
     let estimatedPosition = {
       latitude: currentStop.lat,
@@ -79,8 +80,8 @@ export function computeScheduled(resource: GtfsResource, properties: GtfsPropert
       const estimatedTraveledDistance =
         currentStopTime.distanceTraveled! +
         (nextStopTime.distanceTraveled! - currentStopTime.distanceTraveled!) * timePercentToNextStop;
-      const currentShapePoint = [...trip.shape.points]
-        .reverse()
+      const currentShapePoint = trip.shape.points
+        .toReversed()
         .find((point) => point.distance < estimatedTraveledDistance);
       if (currentShapePoint) {
         const nextShapePoint = trip.shape.points[trip.shape.points.indexOf(currentShapePoint) + 1];
@@ -150,8 +151,8 @@ export async function fetchTripUpdate(resource: GtfsResource, properties: GtfsPr
       })
       .map(async (tripUpdate) => {
         // if (dayjs().diff(dayjs.unix(+tripUpdate.tripUpdate.timestamp), "minutes") >= 10) return;
-        const trip = resource.trips.get(tripUpdate.tripUpdate.trip.tripId);
-        if (typeof trip === 'undefined') return;
+        const trip = search(resource.trips, tripUpdate.tripUpdate.trip.tripId);
+        if (trip === null) return;
         if (
           typeof tripUpdate.tripUpdate.trip.routeId !== 'undefined' &&
           tripUpdate.tripUpdate.trip.routeId !== trip.route
@@ -213,7 +214,7 @@ export async function fetchTripUpdate(resource: GtfsResource, properties: GtfsPr
         });
 
         const currentStopTime =
-          [...stopTimes].reverse().find((stopTime) => {
+          stopTimes.toReversed().find((stopTime) => {
             return dayjs().isAfter(dayjs.unix(stopTime.timestamp ?? stopTime.scheduled));
           }) ?? stopTimes[0];
         if (dayjs().diff(dayjs.unix(currentStopTime.timestamp!), 'minutes') > 10) return;
@@ -234,7 +235,7 @@ export async function fetchTripUpdate(resource: GtfsResource, properties: GtfsPr
           : null;
         const ledColor = vehicleId ? await getVehicleLedColor({ operator: source, number: +vehicleId }) : null;
 
-        const currentStop = resource.stops.get(currentStopTime.id)!;
+        const currentStop = search(resource.stops, currentStopTime.id)!;
 
         let estimatedPosition = {
           latitude: currentStop.lat,
@@ -249,8 +250,8 @@ export async function fetchTripUpdate(resource: GtfsResource, properties: GtfsPr
           const estimatedTraveledDistance =
             currentStopTime.distanceTraveled! +
             (nextStopTime.distanceTraveled! - currentStopTime.distanceTraveled!) * timePercentToNextStop;
-          const currentShapePoint = [...trip.shape.points]
-            .reverse()
+          const currentShapePoint = trip.shape.points
+            .toReversed()
             .find((point) => point.distance < estimatedTraveledDistance);
           if (currentShapePoint) {
             const nextShapePoint = trip.shape.points[trip.shape.points.indexOf(currentShapePoint) + 1];
@@ -328,8 +329,8 @@ export async function fetchVehiclePositionAndTripUpdate(resource: GtfsResource, 
       })
       .map(async (vehiclePosition) => {
         if (typeof vehiclePosition.vehicle.trip === 'undefined') return;
-        const trip = resource.trips.get(vehiclePosition.vehicle.trip.tripId);
-        if (typeof trip === 'undefined') return;
+        const trip = search(resource.trips, vehiclePosition.vehicle.trip.tripId);
+        if (trip === null) return;
         if (
           typeof vehiclePosition.vehicle.trip.routeId !== 'undefined' &&
           vehiclePosition.vehicle.trip.routeId !== trip.route
--- yabs/src/yabs/fetcher/gtfs/download-resource.ts
+++ yabs/src/yabs/fetcher/gtfs/download-resource.ts
@@ -1,8 +1,10 @@
+import { sleep } from 'bun';
 import { exec } from 'node:child_process';
 import { join } from 'node:path';
 
 import { groupBy } from '~/utils/group-by';
 import { parseCsv } from '~/utils/parse-csv';
+import { search, searchSort } from '~/utils/search';
 import { Calendar, GtfsProperties, Shape, Stop, Trip } from '~/yabs/fetcher/gtfs/@types';
 
 const $ = (command: string) =>
@@ -18,7 +20,7 @@ export async function downloadStaticResource(properties: GtfsProperties) {
   await $(`wget -t 1 -T 10 -O "${join(tmpdir, 'gtfs.zip')}" "${properties.staticResourceHref}"`);
   await $(`unzip -o ${join(tmpdir, 'gtfs.zip')} -d ${tmpdir}`);
   await $(`rm "${join(tmpdir, 'feed_info.txt')}"`).catch(() => void 0);
-  if (properties.generateShapes) {
+  if (properties.shapesStrategy === 'GENERATE') {
     if (typeof process.env.OSM_PATH === 'undefined') {
       console.warn(`YABS\t${properties.id}\tOSM_PATH environment variable is missing, skipping shapes generation.`);
     } else {
@@ -27,7 +29,12 @@ export async function downloadStaticResource(properties: GtfsProperties) {
       );
     }
   }
-  const [calendars, shapes, stops] = await Promise.all([loadCalendars(tmpdir), loadShapes(tmpdir), loadStops(tmpdir)]);
+  const [calendars, shapes, stops] = await Promise.all([
+    loadCalendars(tmpdir),
+    properties.shapesStrategy === 'IGNORE' ? [] : loadShapes(tmpdir),
+    loadStops(tmpdir),
+  ]);
+
   const trips = await loadTrips(tmpdir, calendars, shapes, stops);
   await $(`rm -r "${tmpdir}"`);
   const resource = { calendars, stops, trips };
@@ -38,16 +45,13 @@ export async function downloadStaticResource(properties: GtfsProperties) {
 // ---
 
 async function loadCalendars(resourcePath: string) {
-  const calendars = await Bun.file(join(resourcePath, 'calendar.txt'))
-    .text()
-    .then(parseCsv)
-    .catch(() => []);
-  const calendarDates = await Bun.file(join(resourcePath, 'calendar_dates.txt'))
-    .text()
-    .then(parseCsv)
-    .catch(() => []);
-  const calendarSet = calendars.reduce((calendars, calendar) => {
-    calendars.set(calendar.service_id, {
+  const calendars: Calendar[] = (
+    await Bun.file(join(resourcePath, 'calendar.txt'))
+      .text()
+      .then(parseCsv)
+      .catch(() => [])
+  )
+    .map<Calendar>((calendar) => ({
       id: calendar.service_id,
       days: [
         !!+calendar.sunday,
@@ -62,21 +66,28 @@ async function loadCalendars(resourcePath: string) {
       whitelist: [],
       from: calendar.start_date,
       to: calendar.end_date,
-    });
-    return calendars;
-  }, new Map<string, Calendar>());
-  calendarDates.forEach((calendarDate) => {
-    if (!calendarSet.has(calendarDate.service_id)) {
-      calendarSet.set(calendarDate.service_id, {
+    }))
+    .toSorted(searchSort);
+
+  (
+    await Bun.file(join(resourcePath, 'calendar_dates.txt'))
+      .text()
+      .then(parseCsv)
+      .catch(() => [])
+  ).forEach((calendarDate) => {
+    let calendar = search(calendars, calendarDate.service_id);
+    if (calendar === null) {
+      calendar = {
         id: calendarDate.service_id,
         days: [false, false, false, false, false, false, false],
         blacklist: [],
         whitelist: [],
         from: '20000101',
         to: '20991231',
-      });
+      };
+      calendars.push(calendar);
     }
-    const calendar = calendarSet.get(calendarDate.service_id)!;
+
     switch (+calendarDate.exception_type) {
       case 1:
         calendar.whitelist.push(calendarDate.date);
@@ -87,57 +98,50 @@ async function loadCalendars(resourcePath: string) {
       default:
     }
   });
-  return calendarSet;
+
+  return calendars;
 }
 
-async function loadShapes(resourcePath: string) {
+async function loadShapes(resourcePath: string): Promise<Shape[]> {
   const shapePoints = await Bun.file(join(resourcePath, 'shapes.txt'))
     .text()
     .then(parseCsv)
     .catch(() => []);
   const shapes = groupBy(shapePoints, (shapePoint) => shapePoint.shape_id);
-  return [...shapes.entries()].reduce((shapes, [key, points]) => {
-    shapes.set(key, {
-      id: key,
+  return [...shapes.entries()]
+    .map(([shapeId, points]) => ({
+      id: shapeId,
       points: points.map((shapePoint) => ({
         lat: +shapePoint.shape_pt_lat,
         lon: +shapePoint.shape_pt_lon,
         sequence: +shapePoint.shape_pt_sequence,
         distance: +shapePoint.shape_dist_traveled,
       })),
-    });
-    return shapes;
-  }, new Map<string, Shape>());
+    }))
+    .toSorted(searchSort);
 }
 
-async function loadStops(resourcePath: string) {
-  const stops = await Bun.file(join(resourcePath, 'stops.txt')).text().then(parseCsv);
-  return stops.reduce((stops, stop) => {
-    stops.set(stop.stop_id, {
+async function loadStops(resourcePath: string): Promise<Stop[]> {
+  return (await Bun.file(join(resourcePath, 'stops.txt')).text().then(parseCsv))
+    .map((stop) => ({
       id: stop.stop_id,
       name: stop.stop_name,
       lat: +stop.stop_lat,
       lon: +stop.stop_lon,
-    });
-    return stops;
-  }, new Map<string, Stop>());
+    }))
+    .toSorted(searchSort);
 }
 
-async function loadTrips(
-  resourcePath: string,
-  calendars: Map<string, Calendar>,
-  shapes: Map<string, Shape>,
-  stops: Map<string, Stop>,
-) {
+async function loadTrips(resourcePath: string, calendars: Calendar[], shapes: Shape[], stops: Stop[]): Promise<Trip[]> {
   const trips = await Bun.file(join(resourcePath, 'trips.txt')).text().then(parseCsv);
   const stopTimes = groupBy(
     await Bun.file(join(resourcePath, 'stop_times.txt')).text().then(parseCsv),
     (stopTime) => stopTime.trip_id,
   );
-  return trips.reduce((trips, trip) => {
-    trips.set(trip.trip_id, {
+  return trips
+    .map((trip) => ({
       id: trip.trip_id,
-      calendar: calendars.get(trip.service_id)!,
+      calendar: search(calendars, trip.service_id)!,
       block: trip.block_id || null,
       route: trip.route_id,
       direction: +trip.direction_id,
@@ -145,13 +149,12 @@ async function loadTrips(
       stops: (stopTimes.get(trip.trip_id) ?? [])
         .map((stopTime) => ({
           sequence: +stopTime.stop_sequence,
-          stop: stops.get(stopTime.stop_id)!,
+          stop: search(stops, stopTime.stop_id)!,
           time: stopTime.departure_time,
           distanceTraveled: stopTime.shape_dist_traveled ? +stopTime.shape_dist_traveled : null,
         }))
         .sort((a, b) => a.sequence - b.sequence),
-      shape: shapes.get(trip.shape_id) ?? null,
-    });
-    return trips;
-  }, new Map<string, Trip>());
+      shape: search(shapes, trip.shape_id),
+    }))
+    .toSorted(searchSort);
 }
