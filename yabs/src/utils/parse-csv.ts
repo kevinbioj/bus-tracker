96310fcc3fb9b671b9ed19188345f772627037d4
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-05-08 11:52:22 +0200

    fix: correct csv parsing

--- yabs/src/utils/parse-csv.ts
+++ yabs/src/utils/parse-csv.ts
@@ -1,14 +1,17 @@
+import { parse } from 'csv-parse';
+
 export async function parseCsv<T = Record<string, string>>(input: string | Buffer) {
-  const [header, ...records] = input
-    .toString()
-    .trimEnd()
-    .split(/\r?\n/)
-    .map((line) => line.trim().split(','));
+  const [header, ...records] = await new Promise<string[][]>((resolve, reject) =>
+    parse(input, { bom: true, skipEmptyLines: true }, (error, records) => {
+      if (error) reject(error);
+      else resolve(records);
+    }),
+  );
   return records.map((values) => {
     const record = {} as Record<string | number, unknown>;
     values.forEach((value, index) => {
       const key = header[index];
-      record[key] = value.replace(/^\"(.*)\"$/, '$1');
+      record[key] = value;
     });
     return record as T;
   });
