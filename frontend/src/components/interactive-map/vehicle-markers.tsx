46171795fad10668a42915a89d5926fe34f5eb78
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-05-25 22:24:24 +0200

    feat: true tooltips on every devices 🥳

--- frontend/src/components/interactive-map/vehicle-markers.tsx
+++ frontend/src/components/interactive-map/vehicle-markers.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { Tooltip } from "react-tooltip";
+
 import { useVehicles } from "~/hooks/useVehicles";
 
 import VehicleMarker from "./vehicle-marker";
 
-export default function VehicleMarkers() {
+type VehicleMarkersProps = { activeMarker: string | undefined; setActiveMarker: (id?: string) => any };
+
+export default function VehicleMarkers({ activeMarker, setActiveMarker }: VehicleMarkersProps) {
   const vehicles = useVehicles();
   return (
     <>
       {vehicles.map((vehicle) => (
-        <VehicleMarker key={vehicle.id} data={vehicle} />
+        <VehicleMarker key={vehicle.id} data={vehicle} activeMarker={activeMarker} setActiveMarker={setActiveMarker} />
       ))}
     </>
   );
