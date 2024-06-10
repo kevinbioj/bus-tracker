af348f408c4b8a3e15e754d8a7fb03319cfdb2df
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-06-10 22:09:50 +0200

    star: update gtfs resource

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -15,7 +15,7 @@ const sources: Source[] = [
     gtfsProperties: {
       id: 'STAR',
       routePrefix: 'STAR',
-      staticResourceHref: 'https://eu.ftp.opendatasoft.com/star/gtfs/GTFS_1_20240531_20240709_20240531152100.zip',
+      staticResourceHref: 'https://eu.ftp.opendatasoft.com/star/gtfs/GTFS_1_20240604_20240716_20240604143252.zip',
       tripUpdateHref: 'https://proxy.transport.data.gouv.fr/resource/star-rennes-integration-gtfs-rt-trip-update',
       vehiclePositionHref:
         'https://proxy.transport.data.gouv.fr/resource/star-rennes-integration-gtfs-rt-vehicle-position',
