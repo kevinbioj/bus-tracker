f823ac818050ee2a9a8cd2b3fa6ac4e023077d30
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-06-19 08:35:45 +0200

    nomad-geo3d: saint-sever skipped

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -289,6 +289,13 @@ const sources: Source[] = [
       allowScheduled: (trip) => trip.service.id !== 'RT_ONLY',
       routePrefix: 'NOMAD',
       getOperator: () => 'NOMAD',
+      mapTripUpdateEntities: (entities) => {
+        for (const entity of entities) {
+          const rouenStSever = entity.tripUpdate.stopTimeUpdate.find((stu) => stu.stopId === '225');
+          if (rouenStSever) rouenStSever.scheduleRelationship = 'SKIPPED';
+        }
+        return entities;
+      },
     },
   },
   {
