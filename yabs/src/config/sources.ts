605208a521fb4dfb9b89128c44c870031b902e0c
Kevin BIOJOUT <k.biojout@gmail.com>
2024-06-20 22:55:26 +0200

    star: update GTFS + add b back

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -15,11 +15,11 @@ const sources: Source[] = [
     gtfsProperties: {
       id: 'STAR',
       routePrefix: 'STAR',
-      staticResourceHref: 'https://eu.ftp.opendatasoft.com/star/gtfs/GTFS_1_20240604_20240716_20240604143252.zip',
+      staticResourceHref: 'https://eu.ftp.opendatasoft.com/star/gtfs/GTFS_1_20240618_20240707_20240618171025.zip',
       tripUpdateHref: 'https://proxy.transport.data.gouv.fr/resource/star-rennes-integration-gtfs-rt-trip-update',
       vehiclePositionHref:
         'https://proxy.transport.data.gouv.fr/resource/star-rennes-integration-gtfs-rt-vehicle-position',
-      allowScheduled: (trip) => trip.route === '4-1001', // métro a uniquement
+      allowScheduled: (trip) => trip.route === '4-1001' || trip.route === '4-1002', // métros a et b uniquement
     },
   },
 ];
