46f45c413542c70f400da537a1865cce366b470e
Kevin BIOJOUT <k.biojout@gmail.com>
2024-07-08 07:58:11 +0200

    astuce: enable RT for 01-07 RG

--- yabs/src/config/sources.ts
+++ yabs/src/config/sources.ts
@@ -39,10 +39,10 @@ const sources: Source[] = [
       },
       allowScheduled: (trip) => {
         if (['89', '322'].includes(trip.route)) return true;
-        // if (trip.route === '01' && ['Stade Diochon PETIT-QUEVILLY', 'Lafayette ROUEN'].includes(trip.headsign))
-        //   return true;
-        // if (trip.route === '07' && ['Hôtel de Ville SOTTEVILLE-LÈS-ROUEN', 'Lafayette ROUEN'].includes(trip.headsign))
-        //   return true;
+        if (trip.route === '01' && ['Stade Diochon PETIT-QUEVILLY', 'Champlain ROUEN'].includes(trip.headsign))
+          return true;
+        if (trip.route === '07' && ['Hôtel de Ville SOTTEVILLE-LÈS-ROUEN', 'Champlain ROUEN'].includes(trip.headsign))
+          return true;
         return false;
       },
       getOperator: (trip) =>
