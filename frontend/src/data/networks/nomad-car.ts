4ff05f467bf92aee4a9a08b0c15a956aa5429ab9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-09-05 07:40:34 +0200

    nomad-car: 407 > Argentan

--- frontend/src/data/networks/nomad-car.ts
+++ frontend/src/data/networks/nomad-car.ts
@@ -2881,22 +2881,6 @@ const nomadCar: Route[] = [
           pages: [{ font: "1508SUPX", text: "BRIOUZE > BAGNOLES" }],
         },
       },
-      {
-        id: ["Briouze > Argentan"],
-        autoHide: true,
-        name: "Argentan",
-        girouette: {
-          ledColor: "WHITE",
-          routeNumber: {
-            backgroundColor: "#5E8786",
-            textColor: "#FFFFFF",
-            outlineColor: "#000000",
-            font: "1508SUPX",
-            text: "423",
-          },
-          pages: [{ font: "1508SUPX", text: "BRIOUZE > ARGENTAN" }],
-        },
-      },
     ],
   },
   {
@@ -5667,7 +5651,7 @@ const nomadCar: Route[] = [
         },
       },
       {
-        id: ["BAGNOLES DE L'ORNE / LA FERTÉ / ARGENTAN"],
+        id: ["BAGNOLES DE L'ORNE / LA FERTÉ / ARGENTAN", "Bagnoles de l'Orne > Argentan"],
         name: "ARGENTAN",
         girouette: {
           routeNumber: {
